3074c1a24641c1faa8d85082f83c7c552088df18
JhohanSebastianCordobaP <scba246@gmail.com>
2026-09-08 15:38:15 -0500

    usersCRUD

---
+++ src/main/java/dto/UserRequestDTO.java
@@ -0,0 +1,9 @@
+package com.sape.safety_for_people.dto;
+
+public record UserRequestDTO(
+        String name,
+        String email,
+        String password,
+        String phoneNumber,
+        Integer roleId
+);
---
+++ src/main/java/dto/UserResponseDTO.java
@@ -0,0 +1,10 @@
+package com.sape.safety_for_people.dto;
+
+public record UserResponseDTO(
+        Integer id,
+        String name,
+        String email,
+        String phoneNumber,
+        Boolean active,
+        Integer roleId
+);
