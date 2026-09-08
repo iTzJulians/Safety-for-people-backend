29ecb1f1f9a315a2f24716582c6ce1a41aca047f
iTzJulians <itzjulian14@gmail.com>
2026-09-08 10:01:39 -0500

    Spring initializr

---
+++ src/main/java/com/sape/safety_for_people/SafetyForPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.sape.safety_for_people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyForPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyForPeopleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sape/safety_for_people/SafetyForPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sape.safety_for_people;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyForPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
