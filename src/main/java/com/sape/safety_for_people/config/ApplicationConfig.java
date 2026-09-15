acbf08a7a86868318e86a39e9e1f4a24d782ebf0
dargarciacol <santigarcua656565@gmail.com>
2026-09-15 10:31:18 -0500

    Se realiza envio de swagger

--- src/main/java/com/sape/safety_for_people/config/ApplicationConfig.java
+++ src/main/java/com/sape/safety_for_people/config/ApplicationConfig.java
@@ -4,8 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @Configuration
 public class ApplicationConfig {
@@ -18,5 +23,9 @@ public class ApplicationConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> new User(username, "", new ArrayList<>());
     }
 }
